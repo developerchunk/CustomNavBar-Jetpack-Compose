d861d5aea621888d14c104f25e8a7209e5162b7d
Aditya Tech Developers <adityatechdevelopers@gmail.com>
2023-06-30 16:58:42 +0530

    Commit

--- app/src/main/java/com/example/customnavigationbar/screen/bottomnav/HomeScreen.kt
+++ app/src/main/java/com/example/customnavigationbar/screen/bottomnav/HomeScreen.kt
@@ -3,19 +3,28 @@ package com.example.customnavigationbar.screen.bottomnav
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
 import com.google.accompanist.pager.rememberPagerState
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun HomeScreen() {
 
+    var tabIndex by remember { mutableStateOf(0) }
     // declaring the names of TabLayout screens
     val tabScreens = listOf("HomeScreen", "MessageScreen")
     val pagerState = rememberPagerState()
@@ -23,6 +32,23 @@ fun HomeScreen() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
+
+        // top bar of TabLayout
+        TabRow(selectedTabIndex = tabIndex,
+            indicator = { tabPositions -> // 3.
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(
+                        pagerState,
+                        tabPositions
+                    )
+                )
+            }) {
+            tabScreens.forEachIndexed { index, title ->
+                Tab(selected = tabIndex == index,
+                    onClick = { tabIndex = index },
+                    text = { Text(text = title) })
+            }
+        }
 
         // the Swipe-able Horizontal Tab Layout
         HorizontalPager(
