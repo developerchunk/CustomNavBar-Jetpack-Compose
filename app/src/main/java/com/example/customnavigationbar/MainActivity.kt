93d228e5a3e312bdaf294974ff57dc7b9ec7e752
Aditya Tech Developers <adityatechdevelopers@gmail.com>
2023-06-30 15:14:48 +0530

    Commit

--- app/src/main/java/com/example/customnavigationbar/MainActivity.kt
+++ app/src/main/java/com/example/customnavigationbar/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.customnavigationbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.example.customnavigationbar.screen.MainScreen
 import com.example.customnavigationbar.ui.theme.CustomNavigationBarTheme
 
--- app/src/main/java/com/example/customnavigationbar/screen/bottomnav/HomeScreen.kt
+++ app/src/main/java/com/example/customnavigationbar/screen/bottomnav/HomeScreen.kt
@@ -1,12 +1,10 @@
 package com.example.customnavigationbar.screen.bottomnav
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 
 @Composable
