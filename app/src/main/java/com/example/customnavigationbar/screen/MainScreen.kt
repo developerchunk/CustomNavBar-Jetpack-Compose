b9417996888c6e164a60ba8c3e25782bf9db3bd4
Aditya Tech Developers <adityatechdevelopers@gmail.com>
2023-06-30 15:21:33 +0530

    Commit

--- app/src/main/java/com/example/customnavigationbar/screen/MainScreen.kt
+++ app/src/main/java/com/example/customnavigationbar/screen/MainScreen.kt
@@ -3,7 +3,14 @@ package com.example.customnavigationbar.screen
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -37,7 +44,9 @@ fun MainScreen() {
         backgroundColor = Color.White,
         bottomBar = { BottomBar(navController = navController) }
     ) {
-        BottomNavGraph(navController = navController)
+        Column(modifier = Modifier.fillMaxSize().padding(it)) {
+            BottomNavGraph(navController = navController)
+        }
     }
 }
 
